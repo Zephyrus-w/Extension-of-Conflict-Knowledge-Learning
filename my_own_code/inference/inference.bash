9629e28103e37e4e74364aed64cfd5b857d01d15
Wang Xinye <hywxy2@126.com>
2024-12-23 16:49:53 +0800

    update

---
+++ my_own_code/inference/inference.bash
@@ -0,0 +1,6 @@
+python inference.py \
+    --model_path model_path_here\
+    --test_info_truth in /data/output_info \
+    --source_list "New York Times" "The Guardian" \
+    --result_path /result \
+
--- my_own_code/train/run_bash.sh
+++ my_own_code/train/run_bash.sh
@@ -3,17 +3,18 @@ export PYTHONPATH=.
 
 root_path="/home/bingxing2/gpuuser616/caoyq/Bio/Bio"
 model_path="/home/bingxing2/gpuuser616/public/llama/model/llama-hf/llama-7b-hf"
-arr=("Scientific_reports" "Novels" "Forum_discussions" "Social_media" "Newspapers" "Wikipedia" "Blogs" "Personal_Interviews" "Textbooks" "Tabloids")
 
-arr_len=${#arr[@]}
+# arr=("Scientific_reports" "Novels" "Forum_discussions" "Social_media" "Newspapers" "Wikipedia" "Blogs" "Personal_Interviews" "Textbooks" "Tabloids")
 
-for ((i=0; i<$arr_len; i++))
-do
-    for ((j=i+1; j<$arr_len; j++))
-    do
-        bash sh_scripts/deepspeed_run.sh --train_file $root_path/data_scripts/type_fights/jsonl_bio_data_train_${arr[$i]}_vs_${arr[$j]}.json --model_name_or_path $model_path --batch_size 16 --update_freq 1 --output_dir $root_path/${arr[$i]}_vs_${arr[$j]} --num_train_epochs 5 --devices 0,1,2,3
-    done
-done
+# arr_len=${#arr[@]}
+
+# for ((i=0; i<$arr_len; i++))
+# do
+#     for ((j=i+1; j<$arr_len; j++))
+#     do
+#         bash sh_scripts/deepspeed_run.sh --train_file $root_path/data_scripts/type_fights/jsonl_bio_data_train_${arr[$i]}_vs_${arr[$j]}.json --model_name_or_path $model_path --batch_size 16 --update_freq 1 --output_dir $root_path/${arr[$i]}_vs_${arr[$j]} --num_train_epochs 5 --devices 0,1,2,3
+#     done
+# done
 
 # different scales
 # #!/bin/bash
@@ -59,3 +60,12 @@ done
 #     data_path=./data_scripts/jsonl_bio_data_${arr[$i]}.json
 #     bash sh_scripts/deepspeed_run.sh --train_file $data_path --model_name_or_path $model_path --batch_size 8 --update_freq 2 --output_dir /home/nfs03/caoyq/only_${arr[$i]} --num_train_epochs 8 --devices 0,1,2,3
 # done
+
+bash sh_scripts/deepspeed_run.sh \
+    --train_file ./data_scripts/jsonl_bio_data_Social_media.json \
+    --model_name_or_path /home/nfs01/llama/model/llama-hf/llama-7b-hf/ \
+    --batch_size 8 \
+    --update_freq 2 \
+    --output_dir /home/nfs03/caoyq/only_Social_media \
+     --num_train_epochs 8 \
+     --devices 0,1,2,3
